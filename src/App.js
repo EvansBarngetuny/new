a9f47161859e1c8e00b31bec1fb7b91a66b9cd5b
Martin Metodiev <martin.metodiew@gmail.com>
2021-03-23 23:20:37 +0200

    Add Post component & stylization

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import './components/Header/Header.css';
+import './components/Post/Post.css';
 import Header from "./components/Header/Header";
+import Post from "./components/Post/Post";
 
 function App() {
   return (
     <div className="app-wrapper">
 
       <Header />
+
+      <Post />
+
+      <Post />
 
     </div>
   );
---
+++ src/components/Post/Post.js
@@ -0,0 +1,41 @@
+import Avatar from '@material-ui/core/Avatar';
+
+const Post = () => {
+    return (
+        <section className="post-container">
+            <article className="post-header">
+                <Avatar
+                    className="post-header-avatar"
+                    alt="username"
+                    src=""
+                />
+                <h3 className="post-header-username">username</h3>
+            </article>
+            <article className="post-image">
+                <img src="https://miro.medium.com/max/1404/0*ZeJO8KPaD-tfK2oK.jpeg" alt="post-img"/>
+            </article>
+            <article className="post-content">
+                <p className="post-content-text">
+                    <strong>username</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum, porro!
+                </p>
+            </article>
+            <article className="post-comment-section">
+                <p className="post-content-text">
+                    <strong>
+                        other.user
+                    </strong>
+                    <span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
+                    deleniti dignissimos distinctio eaque eius hic maiores repellendus
+                    sapiente voluptas? Asperiores aspernatur consequuntur fugit mollitia nisi
+                    quasi quidem reprehenderit voluptatem voluptates.
+                    </span>
+                </p>
+            </article>
+            <article className="post-add-comments">
+
+            </article>
+        </section>
+    );
+}
+
+export default Post;

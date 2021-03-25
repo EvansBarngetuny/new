55d3e3e16be6c6fc609dd2871ad9819826121d5a
Martin Metodiev <martin.metodiew@gmail.com>
2021-03-25 16:13:17 +0200

    Add CreateNewPost component & functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from "./components/Header/Header";
 import Newsfeed from "./components/NewsFeed/NewsFeed";
+import CreateNewPost from "./components/CreateNewPost/CreateNewPost";
 
 function App() {
     return (
         <div className="app-wrapper">
 
             <Header/>
+
+            <CreateNewPost username="username"/>
 
             <Newsfeed />
 
--- src/components/NewsFeed/NewsFeed.js
+++ src/components/NewsFeed/NewsFeed.js
@@ -6,18 +6,21 @@ const Newsfeed = () => {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        db.collection('posts').onSnapshot(snapshot => {
-            setPosts(snapshot.docs.map(doc => ({
-                id: doc.id,
-                post: doc.data()
-            })));
-        })
+        // listens for a change in the db and adds the new entries to the state each time it's fired
+        db.collection('posts')
+            .orderBy('timestamp', 'desc')
+            .onSnapshot(snapshot => {
+                setPosts(snapshot.docs.map(doc => ({
+                    id: doc.id,
+                    post: doc.data()
+                })));
+            })
     }, [])
 
     return (
         <div className="newsfeed-container">
             {
-                posts.map(p => <Post key={p.id} post={p.post} />)
+                posts.map(p => <Post key={p.id} post={p.post}/>)
             }
         </div>
     );

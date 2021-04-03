ac94fe8ad93b962a6642bc33e42489ad8ed42eb8
Martin Metodiev <martin.metodiew@gmail.com>
2021-04-03 20:08:42 +0300

    Refactor FilteredNewsFeed component

--- src/components/NewsFeed/FilteredNewsFeed.js
+++ src/components/NewsFeed/FilteredNewsFeed.js
@@ -1,35 +1,17 @@
-import {useContext, useEffect, useState} from "react";
-import {db} from "../../firebase";
+import {useEffect, useState} from "react";
 import Spinner from "../../common/components/Spinner/Spinner";
-import AppCtx from "../../context/AppCtx";
 import GridPost from "../Post/GridPost";
+import {parseDataOnSnapshot} from "../../utils/data";
 
-const FilteredNewsfeed = () => {
+const FilteredNewsfeed = ({fetchData}) => {
     const [posts, setPosts] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
-    const {currentUser} = useContext(AppCtx);
 
     useEffect(() => {
         // listens for a change in the db and adds the new entries to the state each time it's fired
-        setIsLoading(true)
-        const unsubscribe = db.collection('posts')
-            .orderBy('timestamp', 'desc')
-            .where('ownerID', '==', currentUser.uid)
-            .limit(6)
-            .onSnapshot(snapshot => {
-                setIsLoading(false);
-                setPosts(snapshot.docs.map(doc => ({
-                    id: doc.id,
-                    post: doc.data()
-                })));
-            });
+        parseDataOnSnapshot(fetchData, setIsLoading, setPosts);
 
-
-        return () => {
-            unsubscribe()
-        }
-
-    }, []);
+    }, [fetchData]);
 
 
     return (
@@ -45,6 +27,7 @@ const FilteredNewsfeed = () => {
               .filtered-newsfeed-container {
                 display: flex;
                 flex-flow: row wrap;
+                align-items: end;
               }
 
             `}
